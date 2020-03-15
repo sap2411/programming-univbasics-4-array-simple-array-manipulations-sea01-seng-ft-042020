d028aeae0b1a20d4dcc0aff7c35649743e193dac
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-15 22:20:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,3 +14,12 @@ def pop_with_args(array)
   array.pop(2)
 end
 
+def using_shift(array)
+  array.shift(2)
+end
+
+def using_concat(array, array1)
+  array.concat(array1)
+end
+
+def using_insert(a)
